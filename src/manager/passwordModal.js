2352685d4111dd23c11f4f3f18bf61d63a1533bd
Jhandei <jhandei@Jhandeis-MacBook-Pro.local>
2024-03-24 01:21:25 +0530

    added copy and view password button

--- src/manager/passwordModal.js
+++ src/manager/passwordModal.js
@@ -1,15 +1,21 @@
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';  
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { decrypt } from './encryption';
+import { faCopy, faEye, faEyeDropper, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.min.js'
 
 
 const PasswordModal = ({show, handleClose, encryptedPassword}) => {
 
     const [decrypted, setDecrypted] = useState(false); 
     const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
 
     const handleDecryption = () => {
+        setShowPassword(false);
         if(!decrypted) {
             let decryptionKey = document.getElementById("decryptionkey").value;
             if (decryptionKey) {
@@ -21,15 +27,27 @@ const PasswordModal = ({show, handleClose, encryptedPassword}) => {
         }
     }
 
+    const handleKeyUp = (event) => {
+        if(event.keyCode == 13){
+            handleDecryption();
+        }
+    }
+
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText(password);
+        console.log("Password Copied");
+    }
+
     return (
-        <Modal show={show} onHide={handleClose}>
+        <Modal show={show} onHide={handleClose} onKeyUp={handleKeyUp}>
             <Modal.Header closeButton>
                 <Modal.Title>Modal title</Modal.Title>
             </Modal.Header>
 
             <Modal.Body>
-                {!decrypted && (<p>Input your decryption key: <input id="decryptionkey" type='password'></input></p>)}
-                {decrypted && (<p>Your password is: {password.toString()} </p>) }
+                { !decrypted ? <p>Input your decryption key: <input id="decryptionkey" type='password'></input></p> :
+                <><p>Your password is: <input type={showPassword ? "text" : "password"} value={password.toString()} readOnly></input> {showPassword ? <FontAwesomeIcon icon={faEye} onClick={() => setShowPassword(false)} /> : <FontAwesomeIcon icon={faEyeSlash} onClick={() => setShowPassword(true)} />}  <FontAwesomeIcon icon={faCopy} onClick={copyToClipboard}/></p> </>}
+            
             </Modal.Body>
 
             <Modal.Footer>
